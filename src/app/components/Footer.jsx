22d62940aecb62e8ecf0637563caade8fb6f3660
Walter <wpiedrabuena@gmail.com>
2025-11-03 12:27:54 -0300

    varios

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -62,16 +62,20 @@ export default function Footer() {
               </a>
 
               <a
-                href="https://www.linkedin.com/company/the-heritage-guardian/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="icon"
-                aria-label="LinkedIn"
-              >
-                <svg viewBox="0 0 24 24" aria-hidden="true">
-                  <path d="M4.98 3.5C4.98 4.88 3.86 6 2.5 6S0 4.88 0 3.5 1.12 1 2.5 1 4.98 2.12 4.98 3.5zM0 8h5v16H0zM8 8h4.8v2.2h.07C13.62 8.84 15.5 8 17.74 8 22.3 8 24 10.7 24 15.1V24h-5v-7.7c0-1.83-.03-4.18-2.55-4.18-2.55 0-2.94 1.99-2.94 4.05V24H8z" fill="currentColor" />
-                </svg>
-              </a>
+  href="https://www.facebook.com/TheHeritageGuardian/"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="icon"
+  aria-label="Facebook"
+>
+<svg viewBox="0 0 24 24" aria-hidden="true">
+  <path
+    fill="currentColor"
+    d="M13.5 8H15V5.5h-2c-2.4 0-3.5 1.85-3.5 3.5V11H7.5v2.5H9.5V19h2.6v-5.5h2.1L14.5 11H12v-1.7c0-.7.5-1.3 1.5-1.3z"
+  />
+</svg>
+</a>
+
 
               <a
                 href="https://www.instagram.com/nachosaso/?hl=en"
